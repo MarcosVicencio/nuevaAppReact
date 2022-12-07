c1bcd78c9b86ccc982122ccd564d4eeb91c8e267
Marcos Vicencio <marcos.vicencio@alumnos.udg.mx>
2022-12-06 20:54:44 -0600

    Cambios 06-12/2022

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className="text-center ubuntu my-4">
+        Rick and Morty <span className="text-primary">Wiki</span>
+        </h1>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-3"></div>
+          </div>
+        </div>
+      </div>
   );
 }
 
